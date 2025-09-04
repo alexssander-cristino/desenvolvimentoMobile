10c34fcb161f63c4798982a3e284f43d9fe1fbb9
Alexssander <alexssander.cristino@unidavi.edu.br>
2025-09-04 20:23:02 -0300

    commit

--- Site/service_work.js
+++ Site/service_work.js
@@ -3,28 +3,46 @@ const assets = [
   './',
   './index.html',
   './style.css',
-  './banner.jpg'
+  './script.js',
+  './manifest.json',
+  './image/banner2.jpg',
+  './image/hamburguer-1.jpg',
+  './image/hamb2.jpg',
+  './image/hamb3.jpg',
+  './image/hamb4.jpg',
+  './image/icon-192.png',
+  './image/icon-512.png'
 ];
 
 // Instalando e salvando arquivos em cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
+  console.log('📦 Service Worker instalado');
 });
 
-// Ativando service worker
+// Ativando service worker e limpando caches antigos
 self.addEventListener('activate', event => {
-  console.log('Service Worker ativado');
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))
+    )
+  );
+  console.log('⚡ Service Worker ativado e caches antigos limpos');
 });
 
-// Respondendo às requisições
+// Respondendo às requisições com cache-first
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request).then(networkResponse => {
+        return caches.open(cacheName).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
     })
   );
 });
